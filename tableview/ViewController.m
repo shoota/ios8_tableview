0a9887a94208685e04c8ff81a8d0aacb0be62574
Shoota Kumano <shoota.dev@gmail.com>
2015-02-01 00:30:31 +0900

    add tableview on UIViewController, Minimum code

--- tableview/ViewController.m
+++ tableview/ViewController.m
@@ -9,19 +9,53 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    @private
+    NSArray *cellText;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    cellText = @[@"hoge",@"foo",@"bar",@"piyo"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+//
+// UITableViewDelegate
+//
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [cellText count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *cellId = @"FirstViewTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: cellId
+                                                            forIndexPath:indexPath];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]
+                initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    }
+    cell.textLabel.text = cellText[indexPath.row];
+    
+    return cell;
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // 選択されたら選択状態を即解除してハイライトを消す。アニメーションで見た目はウィンク効果がつく。
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
